4594046a3a25ddac41ddd7b5e38402ece966b435
Kinga <kinga.maleszewska@o2.pl>
2024-11-07 11:10:36 +0100

    Storing object detection timestamps in a csv file

--- motion_detector.py
+++ motion_detector.py
@@ -1,12 +1,16 @@
-import cv2, time
+import cv2, time, pandas
+from datetime import datetime
 
 first_frame=None
+status_list=[None,None]
+times=[]
+df=pandas.DataFrame(columns=["Start","End"])
 
 video=cv2.VideoCapture(0)
 
 while True:
     check, frame = video.read()
-
+    status=0
     gray=cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
     gray=cv2.GaussianBlur(gray,(21,21),0)
     
@@ -21,11 +25,18 @@ while True:
     (cnts,_) = cv2.findContours(thresh_frame.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
     for contour in cnts:
-        if cv2.contourArea(contour) < 1000:
+        if cv2.contourArea(contour) < 10000:
             continue
+        status=1
 
         (x, y, w, h) = cv2.boundingRect(contour)
         cv2.rectangle(frame, (x, y), (x+w, y+h), (0,255,0), 3)
+
+    status_list.append(status)
+    if status_list[-1]==1 and status_list[-2]==0:
+        times.append(datetime.now())
+    if status_list[-1]==0 and status_list[-2]==1:
+        times.append(datetime.now())
 
     cv2.imshow("Gray Frame", gray)
     cv2.imshow("Delta Frame", delta_frame)
@@ -35,7 +46,14 @@ while True:
     key=cv2.waitKey(1)
 
     if key==ord('q'):
+        if status==1:
+            times.append(datetime.now())
         break
+
+for i in range(0,len(times),2):
+    df=df._append({"Start":times[i],"End":times[i+1]},ignore_index=True)
+
+df.to_csv("Times.csv")
 
 video.release()
 cv2.destroyAllWindows()
